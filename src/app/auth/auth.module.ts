66945f4285d6d3cffe59459049cdf532b96bd766
israelcruzz <israelcruz.contato@gmail.com>
2024-08-04 15:19:00 -0300

    feat: add jwt register in auth route

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigService } from '@nestjs/config';
+import { Env } from '../utils/env';
 
 @Module({
-  imports: [],
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      global: true,
+      useFactory(config: ConfigService<Env, true>) {
+        const secret = config.get('JWT_SECRET', { infer: true });
+
+        return {
+          secret,
+        };
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
