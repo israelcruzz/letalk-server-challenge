7a3f1321dd22d868edf9c3a09ed6ecf3f71479a8
israelcruzz <israelcruz.contato@gmail.com>
2024-08-04 15:11:31 -0300

    feat: add config module for env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,18 @@ import { UsersModule } from './app/users/users.module';
 import { PrismaService } from './app/services/prisma/prisma.service';
 import { AuthModule } from './app/auth/auth.module';
 import { HashService } from './app/services/hash/hash.service';
+import { ConfigModule } from '@nestjs/config';
+import { envSchema } from './app/utils/env';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    UsersModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService, HashService],
 })
