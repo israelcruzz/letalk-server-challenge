e7ef7b0d053851a3416c54d4726dca66a484c1da
israelcruzz <israelcruz.contato@gmail.com>
2024-08-04 01:46:16 -0300

    feat: add auth service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,13 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { AuthRepositoriesInterface } from './repositories/auth-repositories.interface';
 import { createAccountBodySchemaType } from './dto/create-account-dto';
+import { HashService } from '../services/hash/hash.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly authRepository: AuthRepositoriesInterface) {}
+  constructor(
+    private readonly authRepository: AuthRepositoriesInterface,
+    private readonly hash: HashService,
+  ) {}
 
-  public async signUp(user: createAccountBodySchemaType) { 
-    const account = await this.authRepository.store(user);
+  public async signUp(user: createAccountBodySchemaType) {
+    const userPasswordHash = await this.hash.hashPassword(user.password);
+
+    const userData: createAccountBodySchemaType = {
+      name: user.name,
+      email: user.email,
+      password: userPasswordHash,
+      birthDate: user.birthDate,
+      cpf: user.cpf,
+    };
+
+    const account = await this.authRepository.store(userData);
+
     return account;
   }
 }
