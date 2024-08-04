edcdb83e58b04edf90836205ff63f6eaf489770e
israelcruzz <israelcruz.contato@gmail.com>
2024-08-04 00:19:58 -0300

    feat: add prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './app/users/users.module';
+import { PrismaService } from './app/services/prisma/prisma.service';
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
