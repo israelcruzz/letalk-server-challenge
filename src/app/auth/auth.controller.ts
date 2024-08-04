9aa74e3b7d3ea8182a75ada549a7202bb9222026
israelcruzz <israelcruz.contato@gmail.com>
2024-08-04 15:40:07 -0300

    feat: add guard for jwt

--- src/app/auth/auth.controller.ts
+++ src/app/auth/auth.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
+import { JwtAuthGuard } from '../guards/jwt-auth-guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Get()
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtAuthGuard)
   login() {
     return 'a'
   }
