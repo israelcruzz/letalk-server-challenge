7ad5aecc59d2238f4243ad2feb45f50204c53a82
israelcruzz <israelcruz.contato@gmail.com>
2024-08-06 05:41:57 -0300

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,12 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(port);
 }
 bootstrap();
