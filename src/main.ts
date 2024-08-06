172f19e7b8db67229f70da38a528c8a334dac96e
israelcruzz <israelcruz.contato@gmail.com>
2024-08-06 08:52:47 -0300

    fix: fixing cors main

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { Env } from './app/utils/env';
-import cors from "cors"
+import cors from 'cors';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
-  app.use(cors())
+  app.enableCors({
+    origin: '*',
+    credentials: false,
+  });
+
+  app.use(cors());
 
   const configService = app.get<ConfigService<Env, true>>(ConfigService);
   const port = configService.get('PORT', { infer: true });
@@ -18,12 +22,10 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
-  
+
   const config = new DocumentBuilder()
     .setTitle('Documentação com Swagger - Letalk Challenge Emprestimo')
-    .setDescription(
-      'Documentação com todas as rotas do backend.',
-    )
+    .setDescription('Documentação com todas as rotas do backend.')
     .setVersion('1.0')
     .addTag('users')
     .addBearerAuth()
