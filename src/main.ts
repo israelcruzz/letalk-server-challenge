c880e60edc9f3292e1328607a35ef1b7ec39cae1
israelcruzz <israelcruz.contato@gmail.com>
2024-08-06 06:27:02 -0300

    fix: fixing bugs

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
-
+  
   const config = new DocumentBuilder()
     .setTitle('Documentação com Swagger - Letalk Challenge Emprestimo')
     .setDescription(
